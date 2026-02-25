3119159f5a9d244b83c0c6d5e68b80e794fc2935
Alkazim <muhammedalkazim@gmail.com>
2026-02-25 16:51:52 +0530

    Deploy: rebuild with all WebP asset files included

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "32713cebd111ffef63bd4f1c5e6b2a87
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "2e5b6b2ca119dd755ea5c00aff62017b",
+"flutter_bootstrap.js": "1899f045e0443dd4fb1280ef7dd09b1b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
