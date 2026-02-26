bf0cac566c1e9e6d46e4686eefd8ab6026b0701b
Alkazim <muhammedalkazim@gmail.com>
2026-02-26 14:46:55 +0530

    Fix: include both car images in build for hero section

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -39,7 +39,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "47f725f18c523957c151a18509e519bd
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "9d1dad06b1e0cd6622d4ccc4369de8af",
+"flutter_bootstrap.js": "11df3ad14d9284aea57a14204e2df11c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
