9a60c752405dbdbe95567fc7c2949bb248ec0d67
Alkazim <muhammedalkazim@gmail.com>
2026-02-27 06:10:27 +0530

    Fix: remove loading screen via custom flutter_bootstrap.js template with onEntrypointLoaded

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,14 @@
+﻿{{flutter_js}}
+{{flutter_build_config}}
+
+// Custom bootstrap: load Flutter without any loading indicator.
+// Providing onEntrypointLoaded skips the default progress bar.
+_flutter.loader.load({
+  serviceWorkerSettings: {
+    serviceWorkerVersion: {{flutter_service_worker_version}},
+  },
+  onEntrypointLoaded: async function (engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    await appRunner.runApp();
+  },
+});
