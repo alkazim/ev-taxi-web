c47939351830ae46ad43faed858ec47f819df48f
Alkazim <muhammedalkazim@gmail.com>
2026-03-16 13:33:42 +0530

    Built web
    Branch: main
    Commit: d2074f1134a7902b9651ade183f2018118385280 (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -41,7 +41,7 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 // Providing onEntrypointLoaded skips the default progress bar.
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "724286530",
+    serviceWorkerVersion: "2329402540",
   },
   onEntrypointLoaded: async function (engineInitializer) {
     const appRunner = await engineInitializer.initializeEngine();
